f6aafab879fc10156f372c162d79bd213f73ea33
Wallace <Balaniuc>
2022-01-11 21:02:20 -0500

    [ADD] Completed Example 2

--- Week 2/Example1.cpp
+++ Week 2/Example1.cpp
@@ -6,13 +6,13 @@
 using namespace std;		// Using standard namespace declaration
 
 // Our code goes in here!
-int main()					// Main function (main method)
-{							// Main function opening brace (body)
-	cout << "The following items were top sellers\n";
-	cout << "during the month of January:\n";
-	cout << "Playstation 5\n";
-	cout << "Halo Infinite" << endl;
-	cout << "Minecraft" << endl;
-
-	return 0;				// Tells the operating system the application is done. No issues. 
-}							// Main function closing braced
+//int main()					// Main function (main method)
+//{							// Main function opening brace (body)
+//	cout << "The following items were top sellers\n";
+//	cout << "during the month of January:\n";
+//	cout << "Playstation 5\n";
+//	cout << "Halo Infinite" << endl;
+//	cout << "Minecraft" << endl;
+//
+//	return 0;				// Tells the operating system the application is done. No issues. 
+//}							// Main function closing braced
---
+++ Week 2/Example2.cpp
@@ -0,0 +1,16 @@
+// This program has variables
+#include <iostream>
+using namespace std;
+
+//int main()
+//{
+//	char letter;
+//
+//	letter = 65;
+//	cout << letter << endl;
+//
+//	letter = 66;
+//	cout << letter << endl;
+//
+//	return 0;
+//}
