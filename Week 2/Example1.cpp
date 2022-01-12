e5b4aa04e6bf69dfb9aac8ee5dcd611bde02b80b
Wallace <Balaniuc>
2022-01-11 20:39:25 -0500

    [ADD] Completed Example1.cpp

---
+++ Week 2/Example1.cpp
@@ -0,0 +1,18 @@
+/*
+	A simple C++ program
+*/
+
+#include <iostream>			// Preprocessor directive to include iostream in this file
+using namespace std;		// Using standard namespace declaration
+
+// Our code goes in here!
+int main()					// Main function (main method)
+{							// Main function opening brace (body)
+	cout << "The following items were top sellers\n";
+	cout << "during the month of January:\n";
+	cout << "Playstation 5\n";
+	cout << "Halo Infinite" << endl;
+	cout << "Minecraft" << endl;
+
+	return 0;				// Tells the operating system the application is done. No issues. 
+}							// Main function closing braced
