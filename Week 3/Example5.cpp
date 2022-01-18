27999fba806883064b2e9fe1c2f709d158898bfb
Wallace <Balaniuc>
2022-01-18 15:28:26 -0500

    [ADD] Example 6 - How to pause a program.

--- Week 3/Example5.cpp
+++ Week 3/Example5.cpp
@@ -3,21 +3,21 @@
 #include <string>
 using namespace std;
 
-int main()
-{
-	string name;
-	string city;
-
-	// cin unable to read long string containing blanks.
-	cout << "Please enter your name: ";
-	// cin >> name;							
-	getline(cin, name);
-	cout << "Enter the city you live in: ";
-	// cin >> city;
-	getline(cin, city);
-
-	cout << "Hello, " << name << endl;
-	cout << "You live in  " << city << endl;
-
-	return 0;
-}
+//int main()
+//{
+//	string name;
+//	string city;
+//
+//	// cin unable to read long string containing blanks.
+//	cout << "Please enter your name: ";
+//	// cin >> name;							
+//	getline(cin, name);
+//	cout << "Enter the city you live in: ";
+//	// cin >> city;
+//	getline(cin, city);
+//
+//	cout << "Hello, " << name << endl;
+//	cout << "You live in  " << city << endl;
+//
+//	return 0;
+//}
