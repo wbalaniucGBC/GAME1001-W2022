d3c661b9081a0cd470584c5493a357db6cceea1a
Wallace <Balaniuc>
2022-01-18 15:07:00 -0500

    [MOD] Example 2 - Include const PI reference

--- Week 3/Example2.cpp
+++ Week 3/Example2.cpp
@@ -3,20 +3,21 @@
 #include <cmath>	// Mathematics library containing math functions
 using namespace std;
 
-//int main()
-//{
-//	double area, radius;
-//
-//	cout << "This program calculates the area of a circle.\n";
-//	cout << "==============================================\n";
-//
-//	// Get the radius
-//	cout << "What is the radius of the circle? ";
-//	cin >> radius;
-//
-//	// Compute and display the area
-//	area = 3.14159 * pow(radius, 2);
-//	cout << "The area is " << area << endl;
-//
-//	return 0;
-//}
+int main()
+{
+	const double PI = 3.14159;
+	double area, radius;
+
+	cout << "This program calculates the area of a circle.\n";
+	cout << "==============================================\n";
+
+	// Get the radius
+	cout << "What is the radius of the circle? ";
+	cin >> radius;
+
+	// Compute and display the area
+	area = PI * pow(radius, 2);
+	cout << "The area is " << area << endl;
+
+	return 0;
+}
