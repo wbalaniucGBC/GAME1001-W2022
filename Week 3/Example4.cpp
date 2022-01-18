e7501bb63c5b1aa73104d740cdfa91ab29a22e18
Wallace <Balaniuc>
2022-01-18 15:18:18 -0500

    [MOD] Example 4 - Demo'd showpoint, setprecision and fixed.

--- Week 3/Example4.cpp
+++ Week 3/Example4.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 int main()
 {
+	/*
 	int num1 = 2897, num2 = 5,    num3 = 837,
 		num4 = 34,   num5 = 7,    num6 = 1623,
 		num7 = 390,  num8 = 3456, num9 = 12;
@@ -19,6 +20,14 @@ int main()
 
 	// Display the third row of numbers
 	cout << setw(6) << num7 << " " << setw(6) << num8 << " " << setw(6) << num9 << endl;
+	*/
+
+	double x = 6.0;
+
+	cout << x << endl;
+	cout << showpoint << x << endl;			// Enables decimals to be shown in output
+	cout << setprecision(2) << x << endl;	// Sets value to be displayed to 2 significant digits (no fixed used)
+	cout << fixed << x << endl;				// Sets value to be displayed to 2 decimal points (fixed used)
 
 	return 0;
 }
