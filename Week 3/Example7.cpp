69d129d9a4a416d7663f901474a7c813152c8c2a
Wallace <Balaniuc>
2022-01-18 15:44:00 -0500

    [ADD] Example 7 - Working with C-strings

---
+++ Week 3/Example7.cpp
@@ -0,0 +1,15 @@
+// Working with C-string
+#include <iostream>
+using namespace std;
+
+//int main()
+//{
+//	const int SIZE = 50;
+//	char name[SIZE];		// Name is a set of 12 memory "cells"  |W |A |L |L |A |C |E | | B| A| L| '\0'|
+//
+//	cout << "Please enter your first name: ";
+//	cin.getline(name, SIZE);
+//	cout << "Hello, " << name << endl;
+//
+//	return 0;
+//}
