adc87c2ef32b7f838443f08cdc1cf85643be8546
Wallace <Balaniuc>
2022-01-18 14:48:44 -0500

    [ADD] Example1 - Intro to user input

---
+++ Week 3/Example1.cpp
@@ -0,0 +1,22 @@
+// Calculate the area of a rectangle (length x width)
+#include <iostream>
+#include <string>
+using namespace std;
+
+//int main()
+//{
+//	int length, width;
+//	string name;
+//
+//	cout << "This program calculates the area of a rectangle.\n";
+//
+//	// Ask the user to enter the length and width
+//	cout << "What is the length and width and name of the rectangle (separated by a space)? ";
+//	cin >> length >> width >> name;
+//
+//	// Compute the area
+//	cout << "The area of the rectangle is " << length * width << " and is called " << name << endl;
+//
+//
+//	return 0;
+//}
