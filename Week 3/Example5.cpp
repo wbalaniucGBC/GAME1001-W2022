e97ec450b9a3c94cbc0d296efccf4754033bc025
Wallace <Balaniuc>
2022-01-18 15:35:17 -0500

    [MOD] Example 5 - String length and assign actions

--- Week 3/Example5.cpp
+++ Week 3/Example5.cpp
@@ -1,23 +1,46 @@
-// Working with string and C-strings
+// Working with string
 #include <iostream>
 #include <string>
 using namespace std;
 
-//int main()
-//{
-//	string name;
-//	string city;
-//
-//	// cin unable to read long string containing blanks.
-//	cout << "Please enter your name: ";
-//	// cin >> name;							
-//	getline(cin, name);
-//	cout << "Enter the city you live in: ";
-//	// cin >> city;
-//	getline(cin, city);
-//
-//	cout << "Hello, " << name << endl;
-//	cout << "You live in  " << city << endl;
-//
-//	return 0;
-//}
+int main()
+{
+	/*
+	string name;
+	string city;
+
+	// cin unable to read long string containing blanks.
+	cout << "Please enter your name: ";
+	// cin >> name;							
+	getline(cin, name);
+	cout << "Enter the city you live in: ";
+	// cin >> city;
+	getline(cin, city);
+
+	cout << "Hello, " << name << endl;
+	cout << "You live in  " << city << endl;
+	*/
+
+	string firstName, lastName, fullName;
+	string stars;
+	int numStars;
+
+	cout << "Please enter your first name: ";
+	getline(cin, firstName);
+
+	cout << "Please enter your last name: ";
+	getline(cin, lastName);
+
+	// String concatenation ("Combining two strings")
+	fullName = firstName + " " + lastName;
+
+	numStars = fullName.length();	// Return number of characters including blanks
+	stars.assign(numStars, '-');
+
+	cout << endl;
+	cout << stars << endl;
+	cout << fullName << endl;
+	cout << stars << endl;
+
+	return 0;
+}
--- Week 3/Example6.cpp
+++ Week 3/Example6.cpp
@@ -2,25 +2,25 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	char ch;
-
-	/*
-	cout << "Type a character and press Enter: ";
-	cin >> ch;
-	cout << "You entered " << ch << endl;
-	*/
-	cout << "This program has paused. Press Enter to continue.";
-	cin.get(ch);
-
-	cout << "It has paused a second time. Please press Enter again.";
-	ch = cin.get();
-
-	cout << "It has paused a third time. Please press Enter again.";
-	cin.get();
-
-	cout << "Thank you! \n";
-
-	return 0;
-}
+//int main()
+//{
+//	char ch;
+//
+//	/*
+//	cout << "Type a character and press Enter: ";
+//	cin >> ch;
+//	cout << "You entered " << ch << endl;
+//	*/
+//	cout << "This program has paused. Press Enter to continue.";
+//	cin.get(ch);
+//
+//	cout << "It has paused a second time. Please press Enter again.";
+//	ch = cin.get();
+//
+//	cout << "It has paused a third time. Please press Enter again.";
+//	cin.get();
+//
+//	cout << "Thank you! \n";
+//
+//	return 0;
+//}
