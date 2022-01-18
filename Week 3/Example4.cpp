89e90a6ab1ddf4ae861562e300ed58d06a92fd1f
Wallace <Balaniuc>
2022-01-18 15:22:55 -0500

    [ADD] Example 5 - cin vs getline

--- Week 3/Example4.cpp
+++ Week 3/Example4.cpp
@@ -5,29 +5,29 @@ using namespace std;
 
 // setw(6) - | | | | | | |
 
-int main()
-{
-	/*
-	int num1 = 2897, num2 = 5,    num3 = 837,
-		num4 = 34,   num5 = 7,    num6 = 1623,
-		num7 = 390,  num8 = 3456, num9 = 12;
-
-	// Display the first row of numbers
-	cout << left << setw(6) << num1 << " " << setw(6) << num2 << " " << setw(6) << num3 << endl;
-
-	// Display the second row of numbers
-	cout << setw(6) << num4 << " " << setw(6) << num5 << " " << setw(6) << num6 << endl;
-
-	// Display the third row of numbers
-	cout << setw(6) << num7 << " " << setw(6) << num8 << " " << setw(6) << num9 << endl;
-	*/
-
-	double x = 6.0;
-
-	cout << x << endl;
-	cout << showpoint << x << endl;			// Enables decimals to be shown in output
-	cout << setprecision(2) << x << endl;	// Sets value to be displayed to 2 significant digits (no fixed used)
-	cout << fixed << x << endl;				// Sets value to be displayed to 2 decimal points (fixed used)
-
-	return 0;
-}
+//int main()
+//{
+//	/*
+//	int num1 = 2897, num2 = 5,    num3 = 837,
+//		num4 = 34,   num5 = 7,    num6 = 1623,
+//		num7 = 390,  num8 = 3456, num9 = 12;
+//
+//	// Display the first row of numbers
+//	cout << left << setw(6) << num1 << " " << setw(6) << num2 << " " << setw(6) << num3 << endl;
+//
+//	// Display the second row of numbers
+//	cout << setw(6) << num4 << " " << setw(6) << num5 << " " << setw(6) << num6 << endl;
+//
+//	// Display the third row of numbers
+//	cout << setw(6) << num7 << " " << setw(6) << num8 << " " << setw(6) << num9 << endl;
+//	*/
+//
+//	double x = 6.0;
+//
+//	cout << x << endl;
+//	cout << showpoint << x << endl;			// Enables decimals to be shown in output
+//	cout << setprecision(2) << x << endl;	// Sets value to be displayed to 2 significant digits (no fixed used)
+//	cout << fixed << x << endl;				// Sets value to be displayed to 2 decimal points (fixed used)
+//
+//	return 0;
+//}
---
+++ Week 3/Example5.cpp
@@ -0,0 +1,23 @@
+// Working with string and C-strings
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string name;
+	string city;
+
+	// cin unable to read long string containing blanks.
+	cout << "Please enter your name: ";
+	// cin >> name;							
+	getline(cin, name);
+	cout << "Enter the city you live in: ";
+	// cin >> city;
+	getline(cin, city);
+
+	cout << "Hello, " << name << endl;
+	cout << "You live in  " << city << endl;
+
+	return 0;
+}
