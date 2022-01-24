387ca43f4628e964e88df6694432c389cdc501ce
Wallace <Balaniuc>
2022-01-24 13:57:12 -0500

    [MOD] Example 5 - Nested ifs

--- Week 4/Example5.cpp
+++ Week 4/Example5.cpp
@@ -1,61 +1,56 @@
 // Look at if/else-if statements
+// Look at nested if statements + logical comparisons
 // A, B, C, D, F
 #include <iostream>
 using namespace std;
 
-//int main()
-//{
-//	// Create named constants to hold minimum scores required for each grade letter
-//	const int MIN_A_SCORE = 80,
-//		MIN_B_SCORE = 70,
-//		MIN_C_SCORE = 60,
-//		MIN_D_SCORE = 50,
-//		MIN_POSSIBLE_SCORE = 0;
-//
-//	int testScore;			// Hold a numeric test score
-//	char grade;				// Hold a letter grade
-//	bool goodScore = true;
-//
-//	cout << "Enter your numeric test score and I will\ntell you the letter grade you earned: ";
-//	cin >> testScore;
-//
-//	// Determine the letter grade
-//
-//	if (testScore >= MIN_A_SCORE)
-//	{
-//		grade = 'A';
-//	}
-//	else if(testScore >= MIN_B_SCORE)
-//	{
-//		grade = 'B';
-//	}
-//	else if (testScore >= MIN_C_SCORE)
-//	{
-//		grade = 'C';
-//	}
-//	else if (testScore >= MIN_D_SCORE)
-//	{
-//		grade = 'D';
-//	}
-//	else if (testScore >= MIN_POSSIBLE_SCORE)
-//	{
-//		grade = 'F';
-//	}
-//	else // Trailing else. 
-//	{
-//		goodScore = false;	// The score was below 0
-//	}
-//
-//	// Display the letter grade
-//	if (goodScore)
-//	{
-//		cout << "Your grade is " << grade << endl;
-//	}
-//	else
-//	{
-//		cout << "The score cannot be below zero." << endl;
-//	}
-//
-//
-//	return 0;
-//}
+int main()
+{
+	// Create named constants to hold minimum scores required for each grade letter
+	const int MAX_SCORE = 100,
+		MIN_A_SCORE = 80,
+		MIN_B_SCORE = 70,
+		MIN_C_SCORE = 60,
+		MIN_D_SCORE = 50,
+		MIN_POSSIBLE_SCORE = 0;
+
+	int testScore;			// Hold a numeric test score
+
+	cout << "Enter your numeric test score and I will\ntell you the letter grade you earned: ";
+	cin >> testScore;
+
+	// Check if the input is valid
+	if (testScore >= MIN_POSSIBLE_SCORE && testScore <= MAX_SCORE)
+	{
+		// The score is valid. Determine the letter grade
+		if (testScore >= MIN_A_SCORE)
+		{
+			cout << "Your grade is A.\n";
+		}
+		else if (testScore >= MIN_B_SCORE)
+		{
+			cout << "Your grade is B.\n";
+		}
+		else if (testScore >= MIN_C_SCORE)
+		{
+			cout << "Your grade is C.\n";
+		}
+		else if (testScore >= MIN_D_SCORE)
+		{
+			cout << "Your grade is D.\n";
+		}
+		else // Trailing else. 
+		{
+			cout << "Your grade is F.\n";
+		}
+	}
+	else
+	{
+		// The score is invalid. Inform the user
+		cout << "\nThat is an invalid score. Run the program\n"
+			<< "again and enter a value in the range of\n"
+			<< MIN_POSSIBLE_SCORE << " through " << MAX_SCORE << endl;
+	}
+
+	return 0;
+}
