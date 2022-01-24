ca6fca181c05c05f0eaebd8c82d6329c3605f6d5
Wallace <Balaniuc>
2022-01-24 14:04:13 -0500

    [ADD] Example 7 - Block and scope

---
+++ Week 4/Example7.cpp
@@ -0,0 +1,31 @@
+// Declare two variables of the same name
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number;
+
+	cout << "Enter a number greater than 0: ";
+	cin >> number;
+
+	{
+		int number = 20;
+
+		cout << "Surprise! Random number: " << number << endl;
+	}
+
+
+	if (number > 0)
+	{
+		int number;
+
+		cout << "Now enter another number: ";
+		cin >> number;
+		cout << "The second number you entered was ";
+		cout << number << endl;
+	}
+
+	cout << "Your first number was " << number << endl;
+	return 0;
+}
