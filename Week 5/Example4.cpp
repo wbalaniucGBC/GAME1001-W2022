120cdba5d4a9120f9c19ab82c1381fad1fa95090
Wallace <Balaniuc>
2022-01-31 13:54:30 -0500

    [ADD] Example 4 - do-while intro

---
+++ Week 5/Example4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main()
+{
+	int score1, score2, score3;	// Three test scores
+	double average;				// Average test score
+	char again;					// Loop again? Y or N
+	
+	do
+	{
+		// Get three test scores
+		cout << "\nEnter 3 test scores and I will average them: ";
+		cin >> score1 >> score2 >> score3;
+
+		// Calculate and display the average
+		average = (score1 + score2 + score3) / 3.0;
+		cout << "The average is " << average << endl << endl;
+
+		// Does the suer want to average another set?
+		cout << "Do you want to average another set? (Y/N) ";
+		cin >> again;
+
+	} while (toupper(again) == 'Y');
+
+	return 0;
+}
