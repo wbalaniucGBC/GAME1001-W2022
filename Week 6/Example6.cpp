fd1dcf5313dae42cd659bf4aa0472fbdae7eb21f
Wallace <Balaniuc>
2022-02-08 15:03:09 -0500

    [ADD] Example 6 - String as array of characters

---
+++ Week 6/Example6.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main()
+{
+	char ch;
+	int vowelCount = 0;
+	string sentence;
+
+	cout << "Enter any sentence you wish and I will tell you how many vowels are in it.\n";
+	getline(cin, sentence);
+
+	// Challenge!
+	for (int i = 0; i < sentence.length(); i++)
+	{
+		// Uppercase a copy of the next character and assign it to ch
+		ch = toupper(sentence[i]);
+
+		// If the character is a vowel, increment my count
+		switch (ch)
+		{
+			case 'A':
+			case 'E':
+			case 'I':
+			case 'O':
+			case 'U':
+				vowelCount++;
+		}
+	}
+
+	cout << "There are " << vowelCount << " vowles in the sentence.\n";
+
+	return 0;
+}
