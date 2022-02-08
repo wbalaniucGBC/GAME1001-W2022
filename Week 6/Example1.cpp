0eabea5094c211dff4d01ac4c0bc8cd05a0d0d4c
Wallace <Balaniuc>
2022-02-08 14:09:45 -0500

    [MOD] Example 1 - Added loops for data processing.

--- Week 6/Example1.cpp
+++ Week 6/Example1.cpp
@@ -5,25 +5,41 @@ int main()
 {
 	const int NUM_EMPLOYEES = 6;
 	int hours[NUM_EMPLOYEES];		// Hold hours worked for 6 employees
+	int i;
 
 	// Input the hours worked by each employee
 	cout << "Enter the hours work by " << NUM_EMPLOYEES << " employees: ";
 
+	for (i = 0; i < NUM_EMPLOYEES; i++)
+	{
+		cin >> hours[i];
+	}
+
+	/*
 	cin >> hours[0];
 	cin >> hours[1];
 	cin >> hours[2];
 	cin >> hours[3];
 	cin >> hours[4];
 	cin >> hours[5];
+	*/
 
 	// Display the contents to the user
 	cout << "The hours you entered are: ";
+
+	for (i = 0; i < NUM_EMPLOYEES; i++)
+	{
+		cout << " " << hours[i];
+	}
+
+	/*
 	cout << " " << hours[0];
 	cout << " " << hours[1];
 	cout << " " << hours[2];
 	cout << " " << hours[3];
 	cout << " " << hours[4];
 	cout << " " << hours[5];
+	*/
 
 	return 0;
 }
