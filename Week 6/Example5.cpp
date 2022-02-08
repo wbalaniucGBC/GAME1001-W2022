b325228e24827bbd37b4aca74be48a1c25932ce6
Wallace <Balaniuc>
2022-02-08 14:56:58 -0500

    [MOD] Demo'd input and output to array with range-based for loop

--- Week 6/Example5.cpp
+++ Week 6/Example5.cpp
@@ -5,6 +5,25 @@ using namespace std;
 
 int main()
 {
+	const int SIZE = 5;
+	string consoles[SIZE];
+
+	// Get console names from the user
+	for (string &val : consoles)
+	{
+		cout << "Enter a console name: ";
+		getline(cin, val);
+	}
+
+	// Display the values in the array
+	cout << "\nHere are the values you entered: \n";
+	for (string val : consoles)
+	{
+		cout << val << " ";
+	}
+	cout << endl;
+
+	/*
 	string consoles[] = { "PC", "Playstation 5", "Xbox One X", "Nintendo Switch", "Nintendo 64",
 							"Playstation 4", "Gizmondo", "Ouya", "OnLive", "Atari 2600" };
 
@@ -14,5 +33,6 @@ int main()
 		cout << val << endl;
 	}
 
+	*/
 	return 0;
 }
