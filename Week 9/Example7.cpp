b5b1b40d436eb3656d7e0ea20193301a37ac2164
Wallace <Balaniuc>
2022-03-01 13:52:22 -0500

    [ADD] Example 8 Default arguments

--- Week 9/Example7.cpp
+++ Week 9/Example7.cpp
@@ -4,15 +4,15 @@ using namespace std;
 // Function prototype
 void showStatic();
 
-int main()
-{
-	for (int i = 0; i < 5; i++)
-	{
-		showStatic();
-	}
-	
-	return 0;
-}
+//int main()
+//{
+//	for (int i = 0; i < 5; i++)
+//	{
+//		showStatic();
+//	}
+//	
+//	return 0;
+//}
 
 void showStatic()
 {
---
+++ Week 9/Example8.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// Default arguments
+void displayStars(int starsPerRow = 10, int numRows = 1)
+{
+	// Nested loop. The outer loop controls the rows and the 
+	// inner loop controls the number of stars per row.
+	for (int row = 1; row <= numRows; row++)
+	{
+		for (int star = 1; star <= starsPerRow; star++)
+		{
+			cout << '*';
+		}
+
+		cout << endl;
+	}
+}
+
+int main()
+{
+	displayStars(7, 3);
+	cout << endl;
+	displayStars();
+	cout << endl; 
+	displayStars(5);
+
+	return 0;
+}
