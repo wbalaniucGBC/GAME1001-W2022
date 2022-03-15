c4d7e19ee886d43bc23d2e650f016392a2836b85
Wallace <Balaniuc>
2022-03-15 14:00:34 -0400

    [ADD] Example 2 - Structs and functions

--- Week 11/Example1.cpp
+++ Week 11/Example1.cpp
@@ -30,23 +30,23 @@ struct PetInfo
 	}
 };
 
-int main()
-{
-	// Define a PetInfo structure variable called pet
-	PetInfo pet;
-
-	pet.name = "Duke";
-	pet.type = "Cat";
-	pet.age = 6;
-	pet.cost.food = 300.00;
-	pet.cost.medical = 200.00;
-	pet.cost.license = 7.00;
-
-	// Display the total annual costs for the pet
-	cout << fixed << showpoint << setprecision(2);
-	cout << "Annual costs for my " << pet.age << "-year-old " << pet.type << " "
-		<< pet.name << " are $" << (pet.cost.food + pet.cost.medical +
-			pet.cost.license + pet.cost.misc) << endl;
-
-	return 0;
-}
+//int main()
+//{
+//	// Define a PetInfo structure variable called pet
+//	PetInfo pet;
+//
+//	pet.name = "Duke";
+//	pet.type = "Cat";
+//	pet.age = 6;
+//	pet.cost.food = 300.00;
+//	pet.cost.medical = 200.00;
+//	pet.cost.license = 7.00;
+//
+//	// Display the total annual costs for the pet
+//	cout << fixed << showpoint << setprecision(2);
+//	cout << "Annual costs for my " << pet.age << "-year-old " << pet.type << " "
+//		<< pet.name << " are $" << (pet.cost.food + pet.cost.medical +
+//			pet.cost.license + pet.cost.misc) << endl;
+//
+//	return 0;
+//}
---
+++ Week 11/Example2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+struct InvItem
+{
+	int partNum;
+	string description;
+	int onHand;
+	double price;
+};
+
+// Function prototypes
+InvItem getItemData();
+
+void showItem(const InvItem &);		
+
+int main()
+{
+	InvItem part;
+
+	part = getItemData();
+	showItem(part);
+	
+	return 0;
+}
+
+// getItemData
+InvItem getItemData()
+{
+	InvItem item;
+
+	cout << "Enter the part number: ";
+	cin >> item.partNum;
+	cout << "Enter the part description: ";
+	cin.get();
+	getline(cin, item.description);
+	cout << "Enter the quantity on hand: ";
+	cin >> item.onHand;
+	cout << "Enter the unit price: ";
+	cin >> item.price;
+
+	return item;
+}
+
+void showItem(const InvItem& item)
+{
+	cout << fixed << showpoint << setprecision(2) << endl;
+	cout << "Part Number   : " << item.partNum << endl;
+	cout << "Description   : " << item.description << endl;
+	cout << "Units on Hand : " << item.onHand << endl;
+	cout << "Price         : $" << item.price << endl;
+}
