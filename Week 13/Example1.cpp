e3d62ffc2b1b67e0689a1a887f9520a3d1a5cbf5
Wallace <Balaniuc>
2022-03-29 13:15:04 -0400

    [ADD] Example 2 - Working with array of char

--- Week 13/Example1.cpp
+++ Week 13/Example1.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	// Define variables that point to chars
-	const char* p = nullptr, * q = nullptr;
-
-	// Assign string literals to the pointer
-	p = "Hello ";
-	q = "Wallace";
-
-	// Print the pointers as c-string
-	cout << p << q << endl;
-
-	// Print the pointers as c-strings and as addresses
-	cout << p << " is stored at " << int(p) << endl;
-	cout << q << " is stored at " << int(q) << endl;
-
-	// A string literal can be treated as a pointer!
-	cout << "String literal stored at " << int("literal");
-
-	return 0;
-}
+//int main()
+//{
+//	// Define variables that point to chars
+//	const char* p = nullptr, * q = nullptr;
+//
+//	// Assign string literals to the pointer
+//	p = "Hello ";
+//	q = "Wallace";
+//
+//	// Print the pointers as c-string
+//	cout << p << q << endl;
+//
+//	// Print the pointers as c-strings and as addresses
+//	cout << p << " is stored at " << int(p) << endl;
+//	cout << q << " is stored at " << int(q) << endl;
+//
+//	// A string literal can be treated as a pointer!
+//	cout << "String literal stored at " << int("literal");
+//
+//	return 0;
+//}
---
+++ Week 13/Example2.cpp
@@ -0,0 +1,22 @@
+// Cycle through character array until the null terminator is encountered
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int LENGTH = 80;	// Maximum
+	char line[LENGTH];	// Array
+
+	// Read a string into the character array
+	cout << "Enter a sentence of no more than " << LENGTH - 1 << " characters:\n";
+	cin.getline(line, LENGTH);
+	cout << "The sentence you entered is:\n";
+
+	// Loop through the array and print each character
+	for (int i = 0; line[i] != '\0'; i++)
+	{
+		cout << line[i];
+	}
+
+	return 0;
+}
